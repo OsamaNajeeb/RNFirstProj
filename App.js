5d6904ff8f76fafe417f580803a3202dcefca6de
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-24 10:20:15 +0400

    Part 3

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+
+const App = () => {
+  return(
+    <View>
+      <Text>Dodo</Text>
+      <Text>coco</Text>
+      <Text>Dodo</Text>
+      <Text>fofo</Text>
+      <Text>hoho</Text>
+    </View>
+  )
+}
+export default App;

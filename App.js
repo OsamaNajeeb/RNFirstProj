67d87effa5a470e305b203cee6d1d2f95d54422f
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-24 13:43:24 +0400

    Part 9

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Text, View} from 'react-native';
+import CompanyData from './components/CompanyData';
+
+const name = 'Do';
+let age = 3;
+var email = 'push ups';
+
+function now() {
+  return 'Now!!!';
+}
 
 const App = () => {
+  const [name, setName] = useState('Dodo');
+  const fruit = val => {
+    let age = 30;
+    console.error('number ' + age);
+  };
+
+  function updateName() {
+    setName('BoBo');
+  }
+
   return (
     <View>
-      <Text style={{fontSize: 32}}>React-Native</Text>
-      <Button title="Sd">Sd</Button>
+      <Text style={{fontSize: 20}}>{name}</Text>
+      <Button title="sd" onPress={updateName}></Button>
+
+      {/* <UserData /> */}
+      {/* <Button
+        title="Sd"
+        onPress={() => fruit('Dodo')}
+        color={'magenta'}></Button> */}
+      {/* <CompanyData /> */}
     </View>
   );
 };
+
+const UserData = () => {
+  return (
+    <View>
+      <Text>Name : Dood</Text>
+      <Text>Age : 4</Text>
+      <Text>Email : 12</Text>
+    </View>
+  );
+};
+
 export default App;
---
+++ components/CompanyData.js
@@ -0,0 +1,13 @@
+import {Text, View} from 'react-native';
+
+const CompanyData = () => {
+  return (
+    <View>
+      <Text>Name : Haha</Text>
+      <Text>Age : 5</Text>
+      <Text>Email : 1122</Text>
+    </View>
+  );
+};
+
+export default CompanyData;

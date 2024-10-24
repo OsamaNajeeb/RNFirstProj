432fd578afd984790f000fe51001ba0997451f72
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-24 14:11:04 +0400

    Part 10

--- App.js
+++ App.js
@@ -1,47 +1,22 @@
-import React, {useState} from 'react';
+import {useState} from 'react';
 import {Button, Text, View} from 'react-native';
-import CompanyData from './components/CompanyData';
-
-const name = 'Do';
-let age = 3;
-var email = 'push ups';
-
-function now() {
-  return 'Now!!!';
-}
 
 const App = () => {
-  const [name, setName] = useState('Dodo');
-  const fruit = val => {
-    let age = 30;
-    console.error('number ' + age);
-  };
-
-  function updateName() {
-    setName('BoBo');
-  }
-
+  const [dok, setName] = useState('Dood');
   return (
     <View>
-      <Text style={{fontSize: 20}}>{name}</Text>
-      <Button title="sd" onPress={updateName}></Button>
-
-      {/* <UserData /> */}
-      {/* <Button
-        title="Sd"
-        onPress={() => fruit('Dodo')}
-        color={'magenta'}></Button> */}
-      {/* <CompanyData /> */}
+      <Text style={{fontSize: 20}}>Dodo</Text>
+      <Button title="Update Props" onPress={() => setName('Man')} />
+      <Comp dok={dok} age={29} />
     </View>
   );
 };
 
-const UserData = () => {
+const Comp = props => {
   return (
-    <View>
-      <Text>Name : Dood</Text>
-      <Text>Age : 4</Text>
-      <Text>Email : 12</Text>
+    <View style={{backgroundColor: 'green', padding: 10}}>
+      <Text>{props.dok}</Text>
+      <Text>{props.age}</Text>
     </View>
   );
 };

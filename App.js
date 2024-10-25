0131e581475e6fe6510f3224aabae4ce44d0a456
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-25 12:06:34 +0400

    List with map function | without flatlist

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Button,
   FlatList,
   NativeModules,
+  ScrollView,
 } from 'react-native';
 import style from './themes/style';
 
@@ -14,43 +15,87 @@ const App = () => {
   const users = [
     {
       id: 1,
-      name: 'Anvil',
+      name: 'Abby',
     },
     {
       id: 2,
-      name: 'Sam Hyde',
+      name: 'Liara',
     },
     {
       id: 3,
-      name: 'Peter Griffin',
+      name: 'Aparna',
     },
     {
       id: 4,
-      name: 'Brucie',
+      name: 'Nancy',
+    },
+    {
+      id: 5,
+      name: 'Mizuki',
+    },
+    {
+      id: 6,
+      name: 'Eimaan',
+    },
+    {
+      id: 7,
+      name: 'Red Head Chan',
+    },
+    {
+      id: 8,
+      name: 'Nadia',
+    },
+    {
+      id: 9,
+      name: 'EDI',
     },
     {
       id: 10,
-      name: 'Tony Vercitti',
+      name: 'Anna',
+    },
+    {
+      id: 11,
+      name: 'Umbreen',
+    },
+    {
+      id: 12,
+      name: 'Hana',
     },
   ];
 
   return (
     <View>
-      <FlatList
-        data={users}
-        renderItem={({item}) => (
+      <ScrollView style={{marginBottom: 20}}>
+        {users.map(item => (
           <View style={format.listItem}>
-            <Text style={format.text}>{item.name}</Text>
             <Text style={format.text}>{item.id}</Text>
+            <Text style={format.text}>{item.name}</Text>
           </View>
-        )}
-        keyExtractor={item => item.id.toString()}
-      />
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
 export default App;
+
+const ze_Styles = StyleSheet.create({
+  listItem: {
+    color: 'gold',
+    fontSize: 15,
+    padding: 10,
+    backgroundColor: 'black',
+    // fontWeight: '800',
+    // marginHorizontal: 10,
+    // borderRadius: 20,
+    // height: 60,
+    // textAlign: 'center',
+    // textAlignVertical: 'center',
+    borderColor: 'gold',
+    borderWidth: 1,
+    margin: 10,
+  },
+});
 
 const format = StyleSheet.create({
   listItem: {

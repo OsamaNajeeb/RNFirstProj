feda6df0810828fcfb8ec4e3f8db3d0c1eb9bfbf
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-25 10:48:02 +0400

    Basic Form in React-Native | form with state

--- App.js
+++ App.js
@@ -2,17 +2,52 @@ import React, {useState} from 'react';
 import {TextInput, View, StyleSheet, Text, Button} from 'react-native';
 
 const App = () => {
-  const [name, setName] = useState('Dood');
+  const [name, setName] = useState('');
+  const [email, setE] = useState('');
+  const [password, setP] = useState('');
+  const [display, setDisplace] = useState(false);
+
+  const resetFormData = () => {
+    setDisplace(false);
+    setName('');
+    setE('');
+    setP('');
+  };
+
   return (
     <View>
-      <Text>Hello:{name}</Text>
       <TextInput
         style={format.TextInput}
-        placeholder="Fine?"
-        value={name}
+        placeholder="Enter Name Obsessed Brimstone"
         onChangeText={text => setName(text)}
+        value={name}
       />
-      <Button title="But" onPress={() => setName('')} />
+      <TextInput
+        style={format.TextInput}
+        placeholder="Enter Email Obsessed Brimstone"
+        onChangeText={text => setE(text)}
+        value={email}
+      />
+      <TextInput
+        style={format.TextInput}
+        placeholder="Enter Password Obsessed Brimstone"
+        onChangeText={text => setP(text)}
+        secureTextEntry={true}
+        value={password}
+      />
+      <View style={{margin: 20}}>
+        <Button title="Print Details" onPress={() => setDisplace(true)} />
+      </View>
+      <Button title="Clear Details" color={'green'} onPress={resetFormData} />
+      <View>
+        {display ? (
+          <View>
+            <Text>User Name : {name}</Text>
+            <Text>User Email : {email}</Text>
+            <Text>User Password : {password}</Text>
+          </View>
+        ) : null}
+      </View>
     </View>
   );
 };
@@ -21,17 +56,17 @@ export default App;
 
 const format = StyleSheet.create({
   TextInput: {
-    color: 'red',
-    fontSize: 30,
-    backgroundColor: 'grey',
+    color: 'black',
+    fontSize: 15,
+    backgroundColor: 'white',
     // padding: 20,
     fontWeight: '800',
     marginHorizontal: 10,
     borderRadius: 20,
-    height: 100,
+    height: 60,
     textAlign: 'center',
     textAlignVertical: 'center',
-    borderColor: '#666600',
+    borderColor: 'black',
     borderWidth: 5,
   },
 });

8d6b5a6eb5066cc663705fc8c39669647e93db4a
OsamaNajeeb <20F20753@mec.edu.om>
2024-10-28 13:32:45 +0400

    useEffect hook as ComponentDidUpdate life cycle method

--- App.js
+++ App.js
@@ -16,19 +16,44 @@ import Mistress from './components/Mistress';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  useEffect(() => {
-    console.warn('Bye');
-  }, []);
+  const [data, setData] = useState(100);
+
+  // useEffect(() => {
+  //   console.warn('Obsessed');
+  // }, [count]);
+
+  // useEffect(() => {
+  //   console.warn('Brimstone');
+  // }, [data]);
 
   return (
     <View>
-      <Text style={{fontSize: 30}}>Life Cycle {count}</Text>
+      <Text style={{fontSize: 30}}>
+        {data} O algo {count}
+      </Text>
       <Button title="Update" onPress={() => setCount(count + 1)}></Button>
+      <Button title="Update" onPress={() => setData(data + 1)}></Button>
+      <Users info={{data, count}} />
     </View>
   );
 };
 
 export default App;
+
+const Users = props => {
+  console.warn(props.info);
+  useEffect(() => {
+    console.warn('run this code when data is updated');
+  }, [props.info.data]);
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>data: {props.info.data}</Text>
+      <Text style={{fontSize: 30}}>count: {props.info.count}</Text>
+      {/* <Button title="Update" onPress={() => setCount(count + 1)}></Button>
+      <Button title="Update" onPress={() => setData(data + 1)}></Button> */}
+    </View>
+  );
+};
 
 const format = StyleSheet.create({
   listItem: {
